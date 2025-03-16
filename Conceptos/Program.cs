93be712de1c3b8480349b20884f4bdcc785678eb
Santiago <reyestorressanti@gmail.com>
2025-03-15 22:54:43 -0500

    commit parimpar

---
+++ Conceptos/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingresa un numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+if(numero % 2 == 0)
+{
+    Console.WriteLine("Este numero "+ numero +" es par");
+}
+else
+{
+    Console.WriteLine("Este numero "+ numero +" es impar");
+}
